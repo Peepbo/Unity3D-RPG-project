21833df6b3b5f5fb6e1359115a9a28d5e0f944f1
Peepbo <tngus10778@gmail.com>
2020-12-14 12:39:24 +0900

    ObjectPool fix

--- Unity3D RPG/Assets/Scripts/ObjectPool.cs
+++ Unity3D RPG/Assets/Scripts/ObjectPool.cs
@@ -2,59 +2,120 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class ObjectPoolItem
+{
+    public int amountToPool;
+    public GameObject objectToPool;
+    public bool shouldExpand;
+}
+
 public class ObjectPool : MonoBehaviour
 {
     public static ObjectPool SharedInstance;
 
-    //오브젝트를 담아둘 리스트
+    public List<ObjectPoolItem> itemsToPool;
+
+    //public GameObject objectToPool;
+
+    //public int amountToPool;
+
+    //public bool shouldExpand = true;
+
     public List<GameObject> pooledObjects;
-
-    //리스트에 담아둘 오브젝트(프리펩)
-    public GameObject objectToPool;
-
-    //풀에 넣을 오브젝트 최대 개수
-    public int amountToPool;
 
     private void Awake()
     {
-        //다른 스크립트가 컴포넌트를 가져 오지 않고도
-        //액세스 할 수 있도록 함
         SharedInstance = this;
     }
 
+    // Start is called before the first frame update
     void Start()
     {
         pooledObjects = new List<GameObject>();
 
-        for(int i = 0; i < amountToPool; i++)
+        foreach (ObjectPoolItem item in itemsToPool)
         {
-            //프리펩의 정보를 가지고 복제 후, obj에 할당
-            GameObject obj = Instantiate(objectToPool);
-
-            //화면 상 보이면 안되므로 Active를 false
-            obj.SetActive(false);
-
-            //List에 Add해줌
-            pooledObjects.Add(obj);
-
-            //몇 번? amountToPool 만큼 
+            for (int i = 0; i < item.amountToPool; i++)
+            {
+                GameObject obj = Instantiate(item.objectToPool);
+                obj.SetActive(false);
+                pooledObjects.Add(obj);
+            }
         }
+
+        //for (int i = 0; i < amountToPool; i++)
+        //{
+        //    GameObject obj = Instantiate(objectToPool);
+
+        //    obj.SetActive(false);
+
+        //    pooledObjects.Add(obj);
+        //}
     }
 
-    public GameObject GetPooledObject()
+    public GameObject GetPooledObject(string tag)
     {
-        for(int i = 0; i < pooledObjects.Count; i++)
+        //풀에 존재하는 오브젝트 만큼 for문을 돈다
+        for (int i = 0; i < pooledObjects.Count; i++)
         {
-            //만약 i번째의 object가 Hierarchy에서 활성화가 아니면?
-            if(pooledObjects[i].activeInHierarchy == false)
+
+            //그 오브젝트가 Hierarchy창에서 활성화가 아니며,
+            //내가 매개변수로 입력한 tag와 그 오브젝트의 tag가 같을 때
+            if (pooledObjects[i].activeInHierarchy == false &&
+                pooledObjects[i].tag == tag)
             {
-                //그 오브젝트를 return 한다
+
+                //그 오브젝트를 return
                 return pooledObjects[i];
             }
         }
 
-        //여기까지 오면 위에 for문에서 return이 되지 않았다는 의미이며
-        //null을 리턴한다
+        //여기로 온 이유
+
+        //1. tag를 실수로 잘못 입력했을 때 -> 밑에 foreach로 들어가도 맞는 tag가 없어서 결국 return null로 된다..
+
+        //2. 가져오려는 오브젝트가 다 활성화 상태일 때
+        foreach (ObjectPoolItem item in itemsToPool)
+        {
+
+            //우리가 종류별로 만들어 놓은 오브젝트(프리펩)의 태그랑 내가 매개변수로 넣어준 태그를 검사한다.
+            if (item.objectToPool.tag == tag)
+            {
+
+                //해당 프리펩의 확장성이 true?
+                if (item.shouldExpand == true)
+                {
+                    //생성 후
+                    GameObject obj = Instantiate(item.objectToPool);
+                    obj.SetActive(false);
+                    pooledObjects.Add(obj);
+
+                    //반환
+                    return obj;
+                }
+            }
+        }
+
         return null;
+
+        ////확장성이 true
+        //if (shouldExpand)
+        //{
+        //    //생성 해준 뒤 obj에 할당
+        //    GameObject obj = Instantiate(objectToPool);
+
+        //    //obj를 비활성화 한 뒤
+        //    obj.SetActive(false);
+
+        //    //pool에 add해주고
+        //    pooledObjects.Add(obj);
+
+        //    //해당 obj를 return
+        //    return obj;
+        //}
+
+        ////확장성이 false
+        //else return null;
     }
 }
