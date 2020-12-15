54b024e2764c3df52d8cfec4ec71afcf62744ef8
Peepbo <tngus10778@gmail.com>
2020-12-15 15:32:48 +0900

    npc, player, canvas link

---
+++ Unity3D RPG/Assets/Scripts/NPC/openCanvas.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openCanvas : MonoBehaviour
+{
+    public GameObject canvas;
+    public bool isActive = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player") isActive = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player") isActive = false;
+    }
+
+    private void Update()
+    {
+        canvas.SetActive(isActive);
+    }
+}
