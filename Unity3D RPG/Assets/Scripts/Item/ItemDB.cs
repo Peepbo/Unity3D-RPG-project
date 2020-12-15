9e8aae838a44fa45df11352525244006329c364f
ricethief0 <net050722@gmail.com>
2020-12-15 17:02:44 +0900

    add ItemDB Link(Google_sheet)

---
+++ Unity3D RPG/Assets/Scripts/Item/ItemDB.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ItemDB : MonoBehaviour
+{
+    const string URL = "https://docs.google.com/spreadsheets/d/1YuynEfJHxImtJK0brB68n2lLriSeY-y_O9WkFxoI1Wg/export?format=tsv";
+    IEnumerator Start()
+    {
+        UnityWebRequest webLink = UnityWebRequest.Get(URL);
+        yield return webLink.SendWebRequest();
+
+        string data = webLink.downloadHandler.text;
+        print(data);
+    }
+
+    // Update is called once per frame 
+    
+}
