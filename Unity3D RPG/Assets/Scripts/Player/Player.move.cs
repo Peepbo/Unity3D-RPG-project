6e7f9c923722dafd01a9d607e12140d5f2e55118
ahnsem824 <ahnsem96@gmail.com>
2020-12-15 15:31:46 +0900

    Player move

--- Unity3D RPG/Assets/Scripts/Player/Player.move.cs
+++ Unity3D RPG/Assets/Scripts/Player/Player.move.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 
 partial class Player
 {
-    public int a;
+    float hAxis;
+    float vAxis;
+    public float speed;
+    Rigidbody rigid;
+    Vector3 moveVec;
 
+    void GetInput()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        
+    }
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis);
+        if (hAxis != 0 || vAxis != 0)
+        {
+            transform.Translate(moveVec * speed * Time.deltaTime);
+        }
+    }
+    void Turn()
+    {
+       //나중에 
+
+    }
 }
 
