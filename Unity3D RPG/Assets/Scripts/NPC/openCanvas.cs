da3e1434c51648b93035eb654fd98cb55db3eda8
Peepbo <tngus10778@gmail.com>
2020-12-15 23:21:05 +0900

    cinemachine group camera

--- Unity3D RPG/Assets/Scripts/NPC/openCanvas.cs
+++ Unity3D RPG/Assets/Scripts/NPC/openCanvas.cs
@@ -7,12 +7,20 @@ public class openCanvas : MonoBehaviour
 {
     public GameObject canvas;
     public bool isActive = false;
+    Vector3 angle;
 
     public CinemachineVirtualCamera vcam;
+    public CinemachineTargetGroup group;
+    public CinemachineTargetGroup.Target target;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player") isActive = true;
+        if (other.tag == "Player")
+        {
+            isActive = true;
+            group.m_Targets.SetValue(target, 1);
+            //angle = vcam.transform.eulerAngles;
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -20,18 +28,12 @@ public class openCanvas : MonoBehaviour
         if (other.tag == "Player")
         {
             isActive = false;
-            vcam.LookAt = null;
+            group.m_Targets.SetValue(null, 1);
         }
-        
     }
 
     private void Update()
     {
         canvas.SetActive(isActive);
-
-        if(isActive)
-        {
-            vcam.LookAt = transform;
-        }
     }
 }
