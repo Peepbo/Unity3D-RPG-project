b39256cf5fc975d996e6966ef6a8a78c09a19693
Harriet Lee <lharriet27@gmail.com>
2020-12-16 16:19:21 +0900

    Enemy inherit struct

---
+++ Unity3D RPG/Assets/Scripts/Monster/EnemyB.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyB : EnemyManager
+{   
+    
+    protected override void Move()
+    {
+        base.Move();
+    }
+    protected void OnDrawGizmos()
+    {
+        Gizmos.color = Color.Lerp(Color.blue, Color.red, Mathf.PingPong(Time.time, 0.5f));
+        Gizmos.DrawWireSphere(transform.position, findRange);
+    }
+}
