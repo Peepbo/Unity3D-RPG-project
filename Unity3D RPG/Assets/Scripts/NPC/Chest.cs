25f90a77704d35539750acd0e414a7cb46d86449
Peepbo <tngus10778@gmail.com>
2020-12-16 21:11:06 +0900

    make Chest script

---
+++ Unity3D RPG/Assets/Scripts/NPC/Chest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    public List<string> chestItem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        chestItem = new List<string> ();
+
+        PlayerData.Instance.item.Add(ItemCSV.Instance.find("OldNecklace"));
+
+        //PlayerData.Instance.item.Add(ItemCSV.Instance.find("OldArmour"));
+
+        //chestItem.Add(ItemCSV.Instance.find("OldArmour").itemName);
+        //chestItem[1] = ItemCSV.Instance.find("OldArmour").itemName;
+        //chestItem[2] = ItemCSV.Instance.find("OldArmour").itemName;
+        //chestItem[2] = (ItemCSV.Instance.find("OldNecklace").itemName);
+
+        //Debug.Log(chestItem[4]);
+    }
+
+    void chestUpdate()
+    {
+        //for (int i = 0; i < PlayerData.Instance.item.Count; i++)
+        //{
+        //    chestItem.Add(PlayerData.Instance.item[i].itemName);
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Unity3D RPG/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : Singleton<PlayerData>
+{
+    protected PlayerData() { }
+
+    public List<ItemInfo> item = new List<ItemInfo>();
+
+    public string output = "이건되?";
+
+    public void LoadData()
+    {
+        //item.Add(ItemCSV.Instance.find("OldSword"));
+    }
+
+    public void SaveData()
+    {
+
+    }
+}
