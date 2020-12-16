ef8670c730da4f80b0ce0b96ec1bcaa0d332121e
Harriet Lee <lharriet27@gmail.com>
2020-12-16 19:18:00 +0900

    creat monster inherit scripts

--- Unity3D RPG/Assets/Scripts/Monster/EnemyB.cs
+++ Unity3D RPG/Assets/Scripts/Monster/EnemyB.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyB : EnemyManager
-{   
-    
+{ 
+    GameObject arrowPrefab;
+    Transform firePoint;
+
+    private void Awake()
+    {
+        arrowPrefab = GameObject.Find("arrow");
+        firePoint = transform.Find("firePoint");
+    }
     protected override void Move()
     {
         base.Move();
@@ -12,6 +19,15 @@ public class EnemyB : EnemyManager
     protected void OnDrawGizmos()
     {
         Gizmos.color = Color.Lerp(Color.blue, Color.red, Mathf.PingPong(Time.time, 0.5f));
-        Gizmos.DrawWireSphere(transform.position, findRange);
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+    protected override void attack()
+    {
+        //fire 
+        float dis = Vector3.Distance(target.transform.position, transform.position);
+        if (dis < attackRange)
+        {
+            Instantiate(arrowPrefab, target.transform);
+        }
     }
 }
