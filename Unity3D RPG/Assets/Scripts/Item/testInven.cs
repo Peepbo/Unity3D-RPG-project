fb796a5c62e36f29cb63a72d5479b828d24269d9
ricethief0 <net050722@gmail.com>
2020-12-16 16:47:30 +0900

    add CSV 연동

---
+++ Unity3D RPG/Assets/Scripts/Item/testInven.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class testInven : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+
+        Debug.Log(ItemCSV.Instance.find("OldSword").itemName);
+        
+    }
+
+    // Update is called once per frame
+ 
+}
