d1d1ef031371fd5cb5293d743b3415279512662c
DESKTOP-I4K35H7\PONYO <jeongyeon9722@gmail.com>
2020-12-17 12:28:23 +0900

    changed menu panel and achievement panel scale

--- Unity3D RPG/Assets/Scripts/UI/MenuPopUp.cs
+++ Unity3D RPG/Assets/Scripts/UI/MenuPopUp.cs
@@ -28,14 +28,16 @@ public class MenuPopUp : MonoBehaviour
          Time.timeScale = 0f; // 먼저 시간을 정지시킨다. (게임내 시간으로 게임 시간이 정지된다. 애니메이션 , 게임구동 관련 시간)
         // ↑ 이거 쓸지안쓸지 몰라서 일단 넣어놓았음.추후 수정 예정
 
-         menuPanel.SetActive(true);
-         menuIcon.SetActive(false);
+        menuPanel.SetActive(true);
+        menuIcon.SetActive(false);
+        shopIcon.SetActive(false);
     }
 
     public void MenuClose()
     {
         menuPanel.SetActive(false);
         menuIcon.SetActive(true);
+        shopIcon.SetActive(true);
     }
 
     public void AchievementPop()
@@ -59,11 +61,13 @@ public class MenuPopUp : MonoBehaviour
     {
         shopPanel.SetActive(true);
         shopIcon.SetActive(false);
+        menuIcon.SetActive(false);
     }
 
     public void ShopClose()
     {
         shopPanel.SetActive(false);
         shopIcon.SetActive(true);
+        menuIcon.SetActive(true);
     }
 }
