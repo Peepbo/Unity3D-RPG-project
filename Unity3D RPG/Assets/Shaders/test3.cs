c8f9b9cef5b4776fe756f8407abcf59b30b8690f
Peepbo <tngus10778@gmail.com>
2020-12-22 17:52:01 +0900

    add shader, anim function study

---
+++ Unity3D RPG/Assets/Shaders/test3.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test3 : MonoBehaviour
+{
+    public void function()
+    {
+        Debug.Log("HI!");
+    }
+
+    public void function2()
+    {
+        Debug.Log("HI!");
+    }
+}
