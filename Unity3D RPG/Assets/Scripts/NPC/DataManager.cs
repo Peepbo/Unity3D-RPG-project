9e826e0cb8d844b9af0a97dbd7408b7f903ac41e
Peepbo <tngus10778@gmail.com>
2020-12-24 13:48:24 +0900

    playerData revise

--- Unity3D RPG/Assets/Scripts/NPC/DataManager.cs
+++ Unity3D RPG/Assets/Scripts/NPC/DataManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DataManager : MonoBehaviour
 {
-    public StatManager statData;
+    //public StatManager statData;
     public ChestManager chestData;
     // Start is called before the first frame update
     void Start()
