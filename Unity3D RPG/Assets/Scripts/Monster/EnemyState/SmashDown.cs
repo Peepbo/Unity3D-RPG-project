7b240c098f2597d858e87ddaaaa634983c2860aa
Harriet Lee <lharriet27@gmail.com>
2020-12-25 23:17:49 +0900

    goblin prefab create and update, changed enemy manager structure

---
+++ Unity3D RPG/Assets/Scripts/Monster/EnemyState/SmashDown.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmashDown : MonoBehaviour,AttackAble
+{
+    public void attack()
+    {
+        Debug.Log("내려찍기");
+    }
+
+}
