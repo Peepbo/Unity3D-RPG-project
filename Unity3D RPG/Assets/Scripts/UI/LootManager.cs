33944807eb4ef06d4c4bab8627b34b5d09a202f3
DESKTOP-I4K35H7\PONYO <jeongyeon9722@gmail.com>
2020-12-27 02:38:19 +0900

    added loot Icon and panel

---
+++ Unity3D RPG/Assets/Scripts/UI/LootManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootManager : MonoBehaviour
+{
+    public GameObject lootButton;
+    public GameObject lootPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void openLoot()
+    {
+        lootPanel.SetActive(true);
+    }
+
+    public void closeLoot()
+    {
+        lootPanel.SetActive(false);
+    }
+}
