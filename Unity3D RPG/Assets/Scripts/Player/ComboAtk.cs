e09031dde6298e27da62c15eb8fab09245f5a88f
ahnsem824 <ahnsem96@gmail.com>
2020-12-28 02:22:37 +0900

    플레이어 대쉬 버그수정 플레이어 콤보공격 구현 - 약강 약약강 약약약강 강강강 구현완료 (애니메이션 변경해야할듯)

---
+++ Unity3D RPG/Assets/Scripts/Player/ComboAtk.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboAtk : MonoBehaviour
+{
+    public Animator animator;
+    public Player player;
+    bool isCombo;
+    bool isCriCombo;
+    bool isCriAtk;
+    
+    int comboStep;
+    
+    
+    public void Attack() 
+    {
+        if (player.isDash == false)
+        {
+            if (comboStep == 0)
+            {
+                animator.Play("Atk1");
+                comboStep = 1;
+                return;
+            }
+            if (comboStep != 0)
+            {
+                if (isCombo)
+                {
+                    isCombo = false;
+                    comboStep += 1;
+                }
+                
+            }
+        }
+    }
+
+    public void CriAttack()
+    {
+        if (player.isDash == false)
+        {
+            if (comboStep == 0)
+            {
+                
+                animator.Play("Atk1"); //크리공격시작
+                animator.speed = 0.7f;
+                comboStep = 100;
+                return;
+            }
+            if (comboStep != 0)
+            {
+
+                if (isCriAtk)
+                {
+                    isCriAtk = false;
+                    comboStep += 10;
+                }
+            }
+        }
+    }
+    public void ComboPossible()
+    {
+        isCombo = true;
+        isCriAtk = true;
+    }
+
+    public void Combo()
+    {
+        if (comboStep == 2)
+        {
+            animator.Play("Atk2");
+        }
+        if (comboStep == 3)
+        {
+            animator.Play("Atk3");
+        }
+        if (comboStep == 110)
+        {
+            animator.Play("Atk2");
+            animator.speed = 0.7f; 
+        }
+        if (comboStep == 120)
+        {
+            animator.Play("CriAtkFinal");
+        }
+        if (comboStep >10 && comboStep < 100)
+        {
+            animator.Play("Atk4");
+        }
+    }
+
+    public void ComboReset()
+    {
+        isCombo = false;
+        isCriAtk = false;
+        comboStep = 0;
+        animator.speed = 1f;
+    }
+}
--- Unity3D RPG/Assets/Scripts/Player/controller/PlayerController.cs
+++ Unity3D RPG/Assets/Scripts/Player/controller/PlayerController.cs
@@ -50,27 +50,24 @@ public class PlayerController : MonoBehaviour
         {
             playerVelocity.y = 0f;
         }
-        Vector2 _movementInput = playerInput.PlayerMain.Move.ReadValue<Vector2>();
-        value = _movementInput;
-        
-        Vector3 _dis = new Vector3(_movementInput.x, 0f, _movementInput.y);
-        distance = Vector3.Magnitude(_dis);
-
-        Vector3 _move = (cameraMain.forward * _movementInput.y + cameraMain.right * _movementInput.x);
-        _move.y = 0f;
-        value3 = _move;
-
         if (player.isDash == false)
-        {
+        {   
+            Vector2 _movementInput = playerInput.PlayerMain.Move.ReadValue<Vector2>();
+            value = _movementInput;
+            
+            Vector3 _dis = new Vector3(_movementInput.x, 0f, _movementInput.y);
+            distance = Vector3.Magnitude(_dis);
+
+            Vector3 _move = (cameraMain.forward * _movementInput.y + cameraMain.right * _movementInput.x);
+            _move.y = 0f;
+            value3 = _move;
+
+        
             controller.Move(_move * Time.deltaTime * playerSpeed); // 움직임
             child.LookAt(child.position+ _move);
-
         }
-        
+
          playerVelocity.y += gravityValue * Time.deltaTime; //중력적용
          controller.Move(playerVelocity * Time.deltaTime); // 중력적용
-    
-
-        
     }
 }
