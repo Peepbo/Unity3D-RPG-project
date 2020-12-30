639b43b591999c7618ed181709092ce5ff9683d5
Harriet Lee <lharriet27@gmail.com>
2020-12-30 15:03:53 +0900

    flameBall is editing

--- Unity3D RPG/Assets/Scripts/Monster/EnemyState/FlameBall.cs
+++ Unity3D RPG/Assets/Scripts/Monster/EnemyState/FlameBall.cs
@@ -6,6 +6,8 @@ public class FlameBall : MonoBehaviour, IAttackAble
 {
     GameObject target;
     Transform firePos;
+    float smoothTime = 0.3f;
+    float yVelocity = 0.0f;
 
     float time = 0.0f;
     float spawn;
@@ -17,19 +19,25 @@ public class FlameBall : MonoBehaviour, IAttackAble
         firePos = fire;
        
     }
+    IEnumerator attackCycle()
+    {
+        
+        yield return new WaitForSeconds(1f);
+
+       
+    }
+
     public void attack()
     {
         time += Time.deltaTime;
-
         Vector3 _direction = (target.transform.position - transform.position).normalized;
         _direction.y = 0;
-
-        
+        // transform.rotation = Quaternion.LookRotation(_direction);
 
         if (time > spawn)
         {
             time = 0.0f;
-            transform.rotation = Quaternion.LookRotation(_direction);
+
 
             var _firePrefab = ObjectPool.SharedInstance.GetPooledObject("EnemySkill");
 
@@ -37,7 +45,13 @@ public class FlameBall : MonoBehaviour, IAttackAble
             _firePrefab.transform.position = firePos.position;
             _firePrefab.transform.rotation = firePos.rotation;
             _firePrefab.SetActive(true);
-            transform.rotation = Quaternion.LookRotation(_direction);
+
+
+            float newPos = Mathf.SmoothDamp(transform.position.y, target.transform.position.y, ref yVelocity, smoothTime);
+            transform.position = new Vector3(transform.position.x, newPos, transform.position.z);
+            transform.rotation = Quaternion.LookRotation(transform.position);
+
         }
+
     }
 }
