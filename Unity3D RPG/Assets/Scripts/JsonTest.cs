55842e441ab47c3af7ba1e6a88ed3f3cb70aaee5
ricethief0 <net050722@gmail.com>
2021-01-01 08:09:00 +0900

    add JsonTest(암호화)

--- Unity3D RPG/Assets/Scripts/JsonTest.cs
+++ Unity3D RPG/Assets/Scripts/JsonTest.cs
@@ -44,39 +44,39 @@ public class JsonTest : Singleton<JsonTest>
 
     private void Start()
     {
-        character = new CharacterInfo(0, 8,new int[] {-1,-1,-1,-1 } ,
+        character = new CharacterInfo(0, 8, new int[] { -1, -1, -1, -1 },
             new List<SubItem> { new SubItem(1, 2), new SubItem(1, 2) },
             new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             Debug.Log(LoadCurrency());
         }
 
-        if(Input.GetKeyDown(KeyCode.Alpha2))
+        if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             Debug.Log(LoadStaturePoint());
         }
 
-        if(Input.GetKeyDown(KeyCode.Alpha3))
+        if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             Debug.Log(LoadEquip());
         }
 
-        if(Input.GetKeyDown(KeyCode.Alpha4))
+        if (Input.GetKeyDown(KeyCode.Alpha4))
         {
             Debug.Log(LoadItem());
         }
 
-        if(Input.GetKeyDown(KeyCode.Alpha5))
+        if (Input.GetKeyDown(KeyCode.Alpha5))
         {
             Debug.Log(LoadCharacteristic());
         }
 
-        if(Input.GetKeyDown(KeyCode.Y))
+        if (Input.GetKeyDown(KeyCode.Y))
         {
             character.Money += 50;
             Save();
@@ -90,12 +90,14 @@ public class JsonTest : Singleton<JsonTest>
         Debug.Log("저장하기");
 
         JsonData ItemJson = JsonMapper.ToJson(character);
+        byte[] bytes = System.Text.Encoding.UTF8.GetBytes(ItemJson.ToString());
+        string format = System.Convert.ToBase64String(bytes);
         File.WriteAllText(Application.dataPath
             + "/Resources/PlayerData.json"
-            , ItemJson.ToString());
+            , format);
     }
 
-    public void Load()
+    public JsonData Load()
     {
         //Debug.Log("불러오기");
 
@@ -108,43 +110,54 @@ public class JsonTest : Singleton<JsonTest>
         //    Debug.Log(_data["Equip"][i].ToString());
         //}
         //Debug.Log(int.Parse(_data["Money"].ToString()));
+
+        string Jsonstring = File.ReadAllText(Application.dataPath
+            + "/Resources/PlayerData.json");
+        byte[] bytes = System.Convert.FromBase64String(Jsonstring);
+        string reformat = System.Text.Encoding.UTF8.GetString(bytes);
+
+        JsonData _data = JsonMapper.ToObject(reformat);
+
+        return _data; //불러오기 버튼 눌렀을 때 안되는 이유는 void가 아니라 리턴이 포함된 함수이기때문에 안됌. 
+        //따라서 1~5까지 숫자 눌렀을때만 정상작동될수밖에 없는 구조.
     }
 
     public int LoadCurrency()
     {
         Debug.Log("소지금 불러오기");
 
-        string Jsonstring = File.ReadAllText(Application.dataPath
-            + "/Resources/PlayerData.json");
-        JsonData _data = JsonMapper.ToObject(Jsonstring);
+        //string Jsonstring = File.ReadAllText(Application.dataPath
+        //    + "/Resources/PlayerData.json");
+        //JsonData _data = JsonMapper.ToObject(Jsonstring);
+        
 
-        return int.Parse(_data["Money"].ToString());
+        return int.Parse(Load()["Money"].ToString());
     }
 
     public int LoadStaturePoint()
     {
         Debug.Log("성장 포인트 불러오기");
 
-        string Jsonstring = File.ReadAllText(Application.dataPath
-            + "/Resources/PlayerData.json");
-        JsonData _data = JsonMapper.ToObject(Jsonstring);
+        //string Jsonstring = File.ReadAllText(Application.dataPath
+        //    + "/Resources/PlayerData.json");
+        //JsonData _data = JsonMapper.ToObject(Jsonstring);
 
-        return int.Parse(_data["StaturePoint"].ToString());
+        return int.Parse(Load()["StaturePoint"].ToString());
     }
 
     public List<int> LoadEquip()
     {
         Debug.Log("장비 불러오기");
 
-        string Jsonstring = File.ReadAllText(Application.dataPath
-            + "/Resources/PlayerData.json");
-        JsonData _data = JsonMapper.ToObject(Jsonstring);
+        //string Jsonstring = File.ReadAllText(Application.dataPath
+        //    + "/Resources/PlayerData.json");
+        //JsonData _data = JsonMapper.ToObject(Jsonstring);
 
         List<int> _output = new List<int>();
 
-        for(int i = 0; i < _data["Equip"].Count; i++)
+        for (int i = 0; i < Load()["Equip"].Count; i++)
         {
-            _output.Add(int.Parse(_data["Equip"][i].ToString()));
+            _output.Add(int.Parse(Load()["Equip"][i].ToString()));
         }
 
         return _output;
@@ -154,17 +167,17 @@ public class JsonTest : Singleton<JsonTest>
     {
         Debug.Log("소지품 불러오기");
 
-        string Jsonstring = File.ReadAllText(Application.dataPath
-            + "/Resources/PlayerData.json");
-        JsonData _data = JsonMapper.ToObject(Jsonstring);
+        //string Jsonstring = File.ReadAllText(Application.dataPath
+        //    + "/Resources/PlayerData.json");
+        //JsonData _data = JsonMapper.ToObject(Jsonstring);
 
         List<SubItem> _output = new List<SubItem>();
 
-        for (int i = 0; i < _data["Item"].Count; i++)
+        for (int i = 0; i < Load()["Item"].Count; i++)
         {
             _output.Add(new SubItem(
-                int.Parse( _data["Item"][i][0].ToString()),
-                int.Parse( _data["Item"][i][1].ToString())
+                int.Parse(Load()["Item"][i][0].ToString()),
+                int.Parse(Load()["Item"][i][1].ToString())
                 ));
         }
 
@@ -183,15 +196,15 @@ public class JsonTest : Singleton<JsonTest>
     {
         Debug.Log("특성 불러오기");
 
-        string Jsonstring = File.ReadAllText(Application.dataPath
-            + "/Resources/PlayerData.json");
-        JsonData _data = JsonMapper.ToObject(Jsonstring);
+        //string Jsonstring = File.ReadAllText(Application.dataPath
+        //    + "/Resources/PlayerData.json");
+        //JsonData _data = JsonMapper.ToObject(Jsonstring);
 
         List<int> _output = new List<int>();
 
-        for (int i = 0; i < _data["Characteristic"].Count; i++)
+        for (int i = 0; i < Load()["Characteristic"].Count; i++)
         {
-            _output.Add(int.Parse(_data["Characteristic"][i].ToString()));
+            _output.Add(int.Parse(Load()["Characteristic"][i].ToString()));
         }
 
         return _output;
