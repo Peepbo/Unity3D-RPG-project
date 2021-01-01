2fe7b6d3a081e6b3ccf134973d1972a4b2e8eeb8
Harriet Lee <lharriet27@gmail.com>
2021-01-01 18:01:03 +0900

    Adjust angle and create golem prefab

--- Unity3D RPG/Assets/Scripts/Monster/Golem.cs
+++ Unity3D RPG/Assets/Scripts/Monster/Golem.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Golem : EnemyMgr, IDamagedState
 {
     public float dashSpeed;
+    private int findCount;
+    private bool isStay = true;
+    
+    private Vector3 spawnPos;
+    private FollowTarget follow;
+    private ReturnMove returnTohome;
 
-    Vector3 spawnPos;
-    FollowTarget follow;
-    ReturnMove returnTohome;
 
     protected override void Awake()
     {
@@ -32,58 +35,96 @@ public class Golem : EnemyMgr, IDamagedState
         // Vector3 _direction = _between.normalized;
         float _distance = _between.magnitude;
 
-
-        if (_distance < findRange && !returnTohome.getIsReturn())
+        if(isStay)
         {
-
-            FollowTarget();
-
-
+            //집에 있다가 타겟을 찾으면
+            if(_distance <findRange)
+            {
+                isStay = false;
+            }
+            //집에 있다가 타겟을 못찾으면
+            else
+            {
+                Idle();
+            }
         }
-
-        else
+        else 
         {
-            ReturnHome();
+            //집에 없을 때 타겟을 찾으면
+            if (_distance<findRange && !returnTohome.getIsReturn())
+            {
+               
+                //공격 범위에서 공격
+                if (_distance < attackRange)
+                {
+                    AttackTarget();
+                }
+                //공격 범위 전까지 따라감
+                else
+                {
+                    FollowTarget();
+                }
+              
+            }
+            //집에 없을 때 타겟을 못찾으면
+            else
+            {
+                //집으로 가는 중
+                returnTohome.setIsReturn(true);
+                ReturnHome();
+
+            }
+            
         }
 
     }
+    public void Idle()
+    {
+        //집 앞에서 가만히 있는다.
+        Debug.Log("집이닿");
 
+
+    }
     public void FollowTarget()
     {
-        float _home = (spawnPos - transform.position).magnitude;
-
         setMoveType(follow);
         follow.move();
-
-
-        if (_home < 0.1f)
-        {
-            returnTohome.setIsReturn(false);
-        }
 
     }
 
     public void ReturnHome()
     {
+        float _homeDistance = (spawnPos - transform.position).magnitude;
         setMoveType(returnTohome);
         returnTohome.move();
-        returnTohome.setIsReturn(true);
+
+        if(_homeDistance<0.5f)
+        {
+            returnTohome.setIsReturn(false);
+        }
+ 
     }
+ 
+    public void AttackTarget()
+    {
+        Debug.Log("공격");
+    }
+    
     public void Damaged(int value)
     {
-
+        
     }
     public override void Die()
     {
 
     }
 
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.color = Color.yellow;
-    //    Gizmos.DrawWireSphere(spawnPos, 5f);
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(spawnPos, 5f);
 
-    //    Gizmos.color = Color.red;
-    //    Gizmos.DrawWireSphere(transform.position, findRange);
-    //}
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, findRange);
+    }
 }
