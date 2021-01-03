5ece8fc7fe16afbb554580bfc5d77c827d9b1f08
Harriet Lee <lharriet27@gmail.com>
2021-01-03 20:35:36 +0900

    navmeshagent adjust for miniGolem

--- Unity3D RPG/Assets/Scripts/FrameWork/TimeManager.cs
+++ Unity3D RPG/Assets/Scripts/FrameWork/TimeManager.cs
@@ -14,51 +14,56 @@ public class TimeManager : Singleton<TimeManager>
         Time.timeScale += (1f / slowdownLength) * Time.unscaledDeltaTime;
         Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
 
-        if (Input.GetKeyDown(KeyCode.T))
+
+        if(Input.GetKeyDown(KeyCode.T))
         {
-            Debug.Log("t");
-
-            var obj = ObjectPool.SharedInstance.GetPooledObject("Untagged", 1);
-            obj.transform.position = Vector3.zero;
-            obj.transform.rotation = transform.rotation;
-
-            obj.SetActive(true);
-
-            StartCoroutine(coru(obj));
+            DoSlowmotion();
         }
+        //if (Input.GetKeyDown(KeyCode.T))
+        //{
+        //    Debug.Log("t");
 
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-            Debug.Log("y");
+        //    var obj = ObjectPool.SharedInstance.GetPooledObject("Untagged", 1);
+        //    obj.transform.position = Vector3.zero;
+        //    obj.transform.rotation = transform.rotation;
 
-            var obj = ObjectPool.SharedInstance.GetPooledObject("Untagged", 2);
-            obj.transform.position = Vector3.zero;
-            obj.transform.rotation = transform.rotation;
+        //    obj.SetActive(true);
 
-            obj.SetActive(true);
+        //    StartCoroutine(coru(obj));
+        //}
 
-            StartCoroutine(coru(obj));
-        }
+        //if (Input.GetKeyDown(KeyCode.Y))
+        //{
+        //    Debug.Log("y");
 
-        if (Input.GetKeyDown(KeyCode.U))
-        {
-            Debug.Log("u");
+        //    var obj = ObjectPool.SharedInstance.GetPooledObject("Untagged", 2);
+        //    obj.transform.position = Vector3.zero;
+        //    obj.transform.rotation = transform.rotation;
 
-            var obj = ObjectPool.SharedInstance.GetPooledObject("Untagged", 3);
-            obj.transform.position = Vector3.zero;
-            obj.transform.rotation = transform.rotation;
+        //    obj.SetActive(true);
 
-            obj.SetActive(true);
+        //    StartCoroutine(coru(obj));
+        //}
 
-            StartCoroutine(coru(obj));
-        }
+        //if (Input.GetKeyDown(KeyCode.U))
+        //{
+        //    Debug.Log("u");
+
+        //    var obj = ObjectPool.SharedInstance.GetPooledObject("Untagged", 3);
+        //    obj.transform.position = Vector3.zero;
+        //    obj.transform.rotation = transform.rotation;
+
+        //    obj.SetActive(true);
+
+        //    StartCoroutine(coru(obj));
+        //}
     }
 
-    IEnumerator coru(GameObject _obj)
-    {
-        yield return new WaitForSeconds(2f);
-        _obj.SetActive(false);
-    }
+    //IEnumerator coru(GameObject _obj)
+    //{
+    //    yield return new WaitForSeconds(2f);
+    //    _obj.SetActive(false);
+    //}
 
     public void DoSlowmotion()
     {
--- Unity3D RPG/Assets/Scripts/Monster/MiniGolem.cs
+++ Unity3D RPG/Assets/Scripts/Monster/MiniGolem.cs
@@ -7,9 +7,11 @@ public class MiniGolem : EnemyMgr, IDamagedState
 {
     FollowTarget follow;
     ReturnMove back;
+    ViewingAngle view;
 
     private Vector3 spawnPos;
     private Vector3 directionToTarget;
+    private Vector3 findDirection;
     private float distanceBtwTarget;
     private float findDistance;
     private int findCount;
@@ -19,6 +21,7 @@ public class MiniGolem : EnemyMgr, IDamagedState
     public float walkSpeed;
 
     private bool isStay;
+    private bool isReturn;
 
     protected override void Awake()
     {
@@ -30,6 +33,7 @@ public class MiniGolem : EnemyMgr, IDamagedState
     {
         follow = gameObject.AddComponent<FollowTarget>();
         back = gameObject.AddComponent<ReturnMove>();
+        view = gameObject.AddComponent<ViewingAngle>();
         spawnPos = transform.position;
     }
 
@@ -38,64 +42,142 @@ public class MiniGolem : EnemyMgr, IDamagedState
         distanceBtwTarget = (target.transform.position - transform.position).magnitude;
         directionToTarget = (target.transform.position - transform.position).normalized;
 
+        Vector3 _temp = directionToTarget;
+        _temp.y = 0;
 
-        NavMeshHit _hit;
+        
+
+        RaycastHit _rayHit;
+
         //Debug.DrawRay()
 
         if (isStay)
         {
-            //집에 있음
+            Debug.DrawRay(transform.position, _temp * findRange, Color.red);
+
             if (distanceBtwTarget < findRange)
             {
-                isStay = false;
+
+                //Vector3 _myPos = transform.position;
+                //_myPos.y = 0;
+
+
+                if (Physics.Raycast(transform.position, _temp, out _rayHit, findRange))
+                {
+                    //집에 있음
+                    if (_rayHit.transform.tag == "Player")
+                    {
+                        findDirection = target.transform.position;
+                        isStay = false;
+                    }
+
+                    //if (distanceBtwTarget < findRange)
+                    //{
+
+                    //    isStay = false;
+
+                    //}
+                }
             }
         }
+
         else
         {
             if (findCount == 0)
             {
                 //처음 플레이어를 발견했을 때
+                findDistance = (spawnPos - transform.position).magnitude;
 
-                if (!AI.Raycast(target.transform.position, out _hit))
+
+
+                if (findDistance < 20f)
                 {
+                    Debug.Log("돌격!");
+                    AI.speed = dashSpeed;
 
-                    findDistance = (spawnPos - transform.position).magnitude;
+                    AI.stoppingDistance = 0;
+                    AI.SetDestination(findDirection.normalized * 20f);
+
+                    //Debug.DrawRay(transform.position, findDirection.normalized, Color.red);
 
 
-                    if (findDistance < 20f)
+                    if (view.FoundTarget(target, 1.5f, 35f))
                     {
-                        AI.Move(directionToTarget * dashSpeed * Time.deltaTime);
-
-                    }
-                   // else if()
-                    else
-                    {
+                        Debug.Log(target.transform.tag);
+                        //데미지
+                        AI.isStopped = true;
+                        AI.velocity = Vector3.zero;
+                        AI.stoppingDistance = 1;
                         findCount = 1;
                     }
 
+                  
                 }
-               
+
+                else
+                {
+                    AI.stoppingDistance = 1;
+                    Debug.Log("멈춰!");
+
+                    AI.velocity = Vector3.zero;
+                    findCount = 1;
+                }
+
+
+
             }
             else
             {
-                if (distanceBtwTarget > attackRange)
+                if (isReturn == false)
                 {
-                    AI.Move(directionToTarget * walkSpeed * Time.deltaTime);
+                    if (distanceBtwTarget < attackRange)
+                    {
+                        AI.isStopped = true;
+
+                        Debug.Log("target을 attack중");
+                        //attack
+                    }
+                    else
+                    {
+                        AI.isStopped = false;
+                        Debug.Log("target으로 가는 중");
+
+                        AI.speed = walkSpeed;
+                        AI.SetDestination(target.transform.position);
+                    }
                 }
-                else
+
+
+                if (!isReturn && distanceBtwTarget > findRange)
                 {
-                    Debug.Log("공격");
+                    isReturn = true;
+                    AI.SetDestination(spawnPos);
+                    //return
+                    Debug.Log("집으로 return중");
                 }
+
+                if (isReturn)
+                {
+                    Debug.LogError(Vector3.Distance(spawnPos, transform.position));
+
+                    if (Vector3.Distance(spawnPos, transform.position) < 0.5f)
+                    {
+                        Debug.Log("집 도착");
+
+                        isReturn = false;
+                        isStay = true;
+                    }
+                }
+
             }
 
         }
-
     }
-
     public void Damaged(int value)
     {
 
     }
+
     public override void Die()
     {
 
