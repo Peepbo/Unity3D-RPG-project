41fd17e49d1ddb734d19ca19e1cc24d027d5bf38
채수현 <tngus1140@gmail.com>
2021-01-06 12:39:28 +0900

    Dungeon 1 make stoneTrigger

---
+++ Unity3D RPG/Assets/Scripts/Dungeon/RockTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockTrigger : MonoBehaviour
+{
+    public GameObject[] rocks = new GameObject[3];
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                rocks[i].GetComponent<Rigidbody>().useGravity = true;
+            }        
+        }
+    }
+}
