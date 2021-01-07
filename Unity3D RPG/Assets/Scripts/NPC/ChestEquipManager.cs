a88c1dc796e8f513103024207a5e874e84c71833
Peepbo <tngus10778@gmail.com>
2021-01-07 13:42:39 +0900

    chestManager rework finish

--- Unity3D RPG/Assets/Scripts/NPC/ChestEquipManager.cs
+++ Unity3D RPG/Assets/Scripts/NPC/ChestEquipManager.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 
 class ChestEquipManager : MonoBehaviour
 {
-    public int[] myEquip = new int[4]; // ID
+    private int[] myEquip = new int[4]; // ID
 
-    private void Start()
+    [Header("rightPanel")]
+    public Transform rightSlots;
+    public Transform selectPanel;
+    [Space]
+    public int selectNumber;
+
+    private void OnEnable()
     {
         LinkData();
     }
 
     private void LinkData()
     {
+        Debug.Log("Data Load and Link");
         PlayerData.Instance.LoadData_v2();
 
         GetWearing();
         GetEquipInven();
     }
 
-    void GetWearing()
+    private void GetWearing()
     {
         for (int i = 0; i < 4; i++)
         {
@@ -54,31 +61,103 @@ class ChestEquipManager : MonoBehaviour
         }
     }
 
-    void GetEquipInven()
+    private void GetEquipInven()
     {
-        GameObject _rightBg = transform.GetChild(1).gameObject;
-        GameObject _slots = _rightBg.transform.GetChild(0).gameObject;
+        //GameObject _rightBg = transform.GetChild(1).gameObject;
+        //GameObject _slots = _rightBg.transform.GetChild(0).gameObject;
 
-        for (int i = 0; i < _slots.transform.childCount; i++)
+        for (int i = 0; i < rightSlots.childCount; i++)
         {
             if ((i + 1) > PlayerData.Instance.haveEquipItem.Count)
             {
-                _slots.transform.GetChild(i).GetChild(0).GetComponent<Image>().color
+                rightSlots.GetChild(i).GetChild(0).GetComponent<Image>().color
                     = Color.clear;
+                rightSlots.GetChild(i).GetComponent<Button>().enabled = false;
 
                 continue;
             }
 
-            _slots.transform.GetChild(i).GetChild(0).GetComponent<Image>().color
+            rightSlots.GetChild(i).GetChild(0).GetComponent<Image>().color
                     = Color.white;
+            rightSlots.GetChild(i).GetComponent<Button>().enabled = true;
 
-            _slots.transform.GetChild(i).GetChild(0).GetComponent<Image>().sprite
+            rightSlots.GetChild(i).GetChild(0).GetComponent<Image>().sprite
                 = GetPath(PlayerData.Instance.haveEquipItem[i].id);
         }
     }
 
-    Sprite GetPath(int id)
+    private Sprite GetPath(int id)
     {
         return Resources.Load<Sprite>(CSVData.Instance.find(id).iconPath);
     }
+
+    private void ChangeInfo(int id)
+    {
+        ItemInfo _item = CSVData.Instance.find(id);
+
+        List<string> _infos = new List<string>();
+        _infos.Add(_item.itemName);
+
+        switch (_item.kindID)
+        {
+            case 1://무기
+                _infos.Add("공격력 : " + _item.atk);
+                _infos.Add("공격 속도 : " + _item.atkSpeed);
+                break;
+            case 2://갑옷
+                _infos.Add("방어력 : " + _item.def);
+                _infos.Add("");
+                break;
+            case 3://장신구
+                _infos.Add("기술 : " + _item.skill);
+                _infos.Add("");
+                break;
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            selectPanel.GetChild(i).GetComponent<Text>().text = _infos[i];
+        }
+    }
+
+    #region BUTTON ACTIONS
+    public void SelectNumber(int number)
+    {
+        selectNumber = number;
+
+        int _selectId = PlayerData.Instance.haveEquipItem[selectNumber].id;
+
+        ChangeInfo(_selectId);
+    }
+
+    public void ItemWear()
+    {
+        ItemInfo _item = PlayerData.Instance.haveEquipItem[selectNumber];
+        int _id = _item.id;
+        int _kind = _item.kindID;
+
+        int _index = PlayerData.Instance.haveEquipItem.IndexOf(_item);
+
+        if(myEquip[_kind-1] == -1) //착용중인 아이템이 없을 때
+        {
+            PlayerData.Instance.myEquipment[_kind] = _id;
+            PlayerData.Instance.haveEquipItem.RemoveAt(_index);
+        }
+
+        else
+        {
+            int _saveId = PlayerData.Instance.myEquipment[_kind];
+            ItemInfo _saveItem = CSVData.Instance.find(_saveId);
+
+            PlayerData.Instance.myEquipment[_kind] = _id;
+            PlayerData.Instance.haveEquipItem[_index] = _saveItem;
+        }
+
+        PlayerData.Instance.player.EquipStat();
+
+        PlayerData.Instance.SaveData();
+
+        LinkData();
+    }
+    #endregion
 }
---
+++ Unity3D RPG/Assets/chestLootManager.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class chestLootManager : MonoBehaviour
+{
+    public Transform slots;
+
+    [Header("Loot Panel")]
+    public Text currency;
+    [Header("Info Panel")]
+    public Transform itemInfos;
+    [Header("Sell Panel")]
+    public Transform sellInfos;
+    public Slider slider;
+    public Text sellCount;
+    public Text totalPrice;
+    [Space]
+    public int selectNumber;
+
+    private void OnEnable()
+    {
+        LinkData();
+    }
+
+    private void Update()
+    {
+        if(sellInfos.gameObject.activeSelf)
+        {
+            //sellCount
+            sellCount.text = ((int)slider.value).ToString();
+            //price
+            totalPrice.text = (((int)slider.value) * 
+                PlayerData.Instance.haveLootItem[selectNumber].price).ToString();
+        }
+    }
+
+    private void LinkData()
+    {
+        Debug.Log("Data Load and Link");
+        PlayerData.Instance.LoadData_v2();
+
+        GetLoot();
+
+        currency.text = PlayerData.Instance.myCurrency.ToString();
+    }
+
+    private void GetLoot()
+    {
+        for (int i = 0; i < slots.childCount; i++)
+        {
+            Transform _slot = slots.GetChild(i);
+
+            if((i+1) > PlayerData.Instance.haveLootItem.Count )
+            {
+                _slot.GetComponent<Button>().enabled = false;
+
+                _slot.GetChild(0).GetComponent<Image>().color = Color.clear;
+                _slot.GetChild(1).GetComponent<Text>().text = "";
+
+                continue;
+            }
+
+            int _index = PlayerData.Instance.haveLootItem[i].id;
+
+            _slot.GetComponent<Button>().enabled = true;
+
+            //Debug.Log(PlayerData.Instance.haveLootItem[i].count);
+
+            _slot.GetChild(0).GetComponent<Image>().color = Color.white;
+            _slot.GetChild(1).GetComponent<Text>().text =
+                PlayerData.Instance.haveLootItem[i].count.ToString();
+
+            _slot.GetChild(0).GetComponent<Image>().sprite = GetPath(_index);
+        }
+    }
+
+    private void ChangeInfo(ItemInfo item)
+    {
+        List<string> _infos = new List<string>();
+
+        _infos.Add(item.itemName);
+        _infos.Add("가격 : " + item.price.ToString());
+        _infos.Add("소지량 : " + item.count.ToString());
+
+        for(int i = 0; i < 3; i++)
+        {
+            itemInfos.GetChild(i).GetComponent<Text>().text = _infos[i];
+        }
+    }
+
+    private Sprite GetPath(int id)
+    {
+        return Resources.Load<Sprite>(CSVData.Instance.find(id).iconPath);
+    }
+
+
+    #region BUTTON ACTIONS
+    public void GetInfo(int number)
+    {
+        selectNumber = number;
+
+        ItemInfo _item = PlayerData.Instance.haveLootItem[selectNumber];
+
+        ChangeInfo(_item);
+    }
+
+    public void GetSellInfo()
+    {
+        ItemInfo _item = PlayerData.Instance.haveLootItem[selectNumber];
+
+        //value
+        slider.value = 1;
+
+        //max
+        sellInfos.GetChild(1).GetComponent<Text>().text = (_item.count).ToString();
+        slider.maxValue = _item.count;
+        //sellCount
+        sellCount.text = ((int)slider.value).ToString();
+        //price
+        totalPrice.text = (((int)slider.value) * _item.price).ToString();
+    }
+
+    public void SellItem()
+    {
+
+        int _price = PlayerData.Instance.haveLootItem[selectNumber].price;
+
+        PlayerData.Instance.haveLootItem[selectNumber].count -= (int)slider.value;
+
+        if (PlayerData.Instance.haveLootItem[selectNumber].count == 0)
+            PlayerData.Instance.haveLootItem.RemoveAt(selectNumber);
+
+        PlayerData.Instance.myCurrency += _price * ((int)slider.value);
+
+        PlayerData.Instance.SaveData();
+
+        LinkData();
+    }
+    #endregion
+}
