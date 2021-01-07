e3da429d1d5d7e60c1c15c2812bb2d991554a9ff
ricethief0 <net050722@gmail.com>
2021-01-08 05:06:39 +0900

    edit ChiefTain(atk )

--- Unity3D RPG/Assets/Scripts/Boss/BossATKPattern.cs
+++ Unity3D RPG/Assets/Scripts/Boss/BossATKPattern.cs
@@ -2,7 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface BossATKPattern 
+public interface IBossATKPattern 
 {
     void SetupATKPattern(ref Animator anim, Transform target);
+}
+
+public class ThreeATKPattern : IBossATKPattern
+{
+    public void SetupATKPattern(ref Animator anim, Transform target)
+    {
+        anim.SetTrigger("ThreeATK");
+        anim.SetInteger("ThreeATKKind",0);
+      
+    }
 }
