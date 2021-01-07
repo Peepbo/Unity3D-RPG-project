fda8e04b0470dab261f2030d9a9daf9f1dfd3bae
Harriet Lee <lharriet27@gmail.com>
2021-01-08 00:51:44 +0900

    golem damage system change

---
+++ Unity3D RPG/Assets/Scripts/Monster/DamageCheck.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageCheck : MonoBehaviour
+{
+    int damageCount;
+
+    private void Awake()
+    {
+        damageCount = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "HandR")
+        {
+            damageCount = 1;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "HandR"|| other.tag == "Player")
+        {
+            damageCount = 0;
+        }
+    }
+    public void ReadyToDamage(int value)
+    {
+        damageCount = value;
+    }
+
+    public int GetCount() { return damageCount; }
+}
