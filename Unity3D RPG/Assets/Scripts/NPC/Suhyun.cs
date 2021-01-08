db9628926caeaf5fbb9ceb89e18c504d73059057
Peepbo <tngus10778@gmail.com>
2021-01-09 00:50:16 +0900

    make hit effect

--- Unity3D RPG/Assets/Scripts/NPC/Suhyun.cs
+++ Unity3D RPG/Assets/Scripts/NPC/Suhyun.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Suhyun : MonoBehaviour
 {
-    public Transform target;
-
-    public GameObject[] prefab = new GameObject[3];
+    public GameObject prefab;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.T))
         {
-            Instantiate(prefab[0], target.position, Quaternion.identity);
+            //Instantiate(prefab, transform.position, Quaternion.identity);
+            prefab.SetActive(true);
         }
     }
 }
