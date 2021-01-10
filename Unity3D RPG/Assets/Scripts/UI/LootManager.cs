72270ee7ad1794e069cc8096d54990bf8d200ade
Peepbo <tngus10778@gmail.com>
2021-01-10 18:32:28 +0900

    LootManager link insurance

--- Unity3D RPG/Assets/Scripts/UI/LootManager.cs
+++ Unity3D RPG/Assets/Scripts/UI/LootManager.cs
@@ -1,40 +1,15 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-//public class pocketData
-//{
-//    public List<ItemInfo> pocketItem = new List<ItemInfo>();  // 던전에서 획득 한 전리품 (아이템)
-//    public int pocketMoney = 0;                               // 던전에서 획득 한 전리품 (돈)
-//                                                              //public ItemInfo myPocketItem = null;
-//}
-
 public partial class LootManager : Singleton<LootManager>
 {
     protected LootManager() { }
 
-    //public GameObject poccketPanel;
-    //pocketData pd = new pocketData();
     public List<ItemInfo> pocketItem = new List<ItemInfo>();
     public int pocketMoney = 0;
-    //public GameObject checkMoney;
 
     public void GetPocketData(List <ItemInfo> pocketItemInfo)
     {
-        //List<ItemInfo> _itemList = new List<ItemInfo>();
-
-        //ItemInfo _item = CSVData.Instance.find(64); // count = 1
-        //_item.count = 3;
-
-        //_itemList.Add(_item);
-
-        //ItemInfo _item0 = CSVData.Instance.find(52);
-        
-        //_itemList.Add(_item);
-
-        //GetPocketData(_itemList);
-
-
-        //itemA, itemB, itemC, itemD...
         for(int i = 0; i < pocketItemInfo.Count; i++)
         {
             ItemInfo _item = new ItemInfo();
@@ -48,8 +23,6 @@ public partial class LootManager : Singleton<LootManager>
                 pocketItem[_index].count += _item.count;
             }
         }
-
-        //checkMoney.transform.GetChild(1).GetComponent<Text>().text = pocketMoney.ToString();
     }
 
     public void GetPocketMoney(int currency)
@@ -57,21 +30,47 @@ public partial class LootManager : Singleton<LootManager>
         pocketMoney += currency;
     }
 
-    public void Delivery()
+    public void Delivery(bool die)
     {
-        //for (int i = 0; i < pd.pocketItem.Count; i++)
-        //{
-        //    for (int j = 0; j < pd.pocketItem[i].count; j++)
-        //    {
-        //        PlayerData.Instance.SaveChest(pd.pocketItem[i].id);
-        //    }
-        //}
-        for (int i = 0; i < pocketItem.Count; i++)
+        if(die)
         {
-            PlayerData.Instance.SaveChest(pocketItem[i].id, pocketItem[i].count);
+            PlayerData.Instance.myCurrency += pocketMoney;
+
+            if(PlayerData.Instance.normalInsurance)
+            {
+                for (int i = 0; i < pocketItem.Count; i++)
+                {
+                    if (pocketItem[i].grade == "normal")
+                    {
+                        PlayerData.Instance.SaveChest(pocketItem[i].id, pocketItem[i].count);
+                    }
+                }
+            }
+
+            if(PlayerData.Instance.rareInsurance)
+            {
+                for (int i = 0; i < pocketItem.Count; i++)
+                {
+                    if (pocketItem[i].grade == "rare")
+                    {
+                        PlayerData.Instance.SaveChest(pocketItem[i].id, pocketItem[i].count);
+                    }
+                }
+            }
         }
 
-        PlayerData.Instance.myCurrency += pocketMoney;
+        else
+        {
+            for (int i = 0; i < pocketItem.Count; i++)
+            {
+                PlayerData.Instance.SaveChest(pocketItem[i].id, pocketItem[i].count);
+            }
+
+            PlayerData.Instance.myCurrency += pocketMoney;
+        }
+
+        PlayerData.Instance.ResetInsurance();
+
         PlayerData.Instance.SaveData();
     }
 
