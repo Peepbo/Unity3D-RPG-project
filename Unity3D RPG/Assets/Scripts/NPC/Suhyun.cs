6766e8fbab0c6068a68c372c2b77114f9c1532e7
Peepbo <tngus10778@gmail.com>
2021-01-12 01:41:07 +0900

    D 1-3 work3

--- Unity3D RPG/Assets/Scripts/NPC/Suhyun.cs
+++ Unity3D RPG/Assets/Scripts/NPC/Suhyun.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Suhyun : MonoBehaviour
 {
-    //public void hi()
-    //{
-    //    LootData.Instance.Something();
-    //}
-    //public void Update()
-    //{
-    //    if(Input.GetKeyDown(KeyCode.Y))
-    //    {
-    //        hi();
-    //    }
-    //}
+    public CinemachineTrackedDolly cam;
+
+    private void Start()
+    {
+        cam = GetComponent<CinemachineTrackedDolly>();
+    }
+
+    public void Update()
+    {
+        if(cam.m_PathPosition < 84f)
+        {
+            cam.m_PathPosition = Mathf.Lerp(cam.m_PathPosition, 84.54f, Time.realtimeSinceStartup);
+        }
+        
+    }
 }
