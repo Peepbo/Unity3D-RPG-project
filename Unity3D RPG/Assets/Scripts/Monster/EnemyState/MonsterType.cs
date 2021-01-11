72adc633421bbec4899cc842d06570c570115f79
Harriet Lee <lharriet27@gmail.com>
2021-01-11 18:05:38 +0900

    Attached Monster type

---
+++ Unity3D RPG/Assets/Scripts/Monster/EnemyState/MonsterType.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MonType
+{
+    Melee, Range
+}
+public class MonsterType : MonoBehaviour
+{
+    public MonType type;
+
+    public MonType GetEnemyType()
+    {
+        return type;
+    }
+
+}
