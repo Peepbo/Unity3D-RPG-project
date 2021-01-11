50e0d0727b405c13ab5560bc91c81c8f0d0f20f3
Peepbo <tngus10778@gmail.com>
2021-01-12 00:27:51 +0900

    D 1-3 work2

--- Unity3D RPG/Assets/Scripts/Dungeon/DungeonQuest.cs
+++ Unity3D RPG/Assets/Scripts/Dungeon/DungeonQuest.cs
@@ -5,6 +5,10 @@ using TMPro;
 
 public class DungeonQuest : MonoBehaviour
 {
+    public bool keyQuest = false;
+    public bool allKillQuest = false;
+    public bool meleeAllKillQuest = false;
+
     public enum QUEST { 
         KEY,
         ALLKILL,
@@ -15,6 +19,7 @@ public class DungeonQuest : MonoBehaviour
 
     #region KEY
     //find key
+    public int boxNumber = 4;
     int correctNumber = 0;
     #endregion
 
@@ -37,13 +42,18 @@ public class DungeonQuest : MonoBehaviour
     {
         isClear = false;
 
+
+
         var _allEnemys = GameObject.FindGameObjectsWithTag("Enemy");
-        MonsterType mt = new MonsterType();
+        Debug.Log(_allEnemys.Length);
+        MonsterType mt;
+
+        quest = ChooseQuest();
 
         switch (Quest)
         {
             case QUEST.KEY:
-                correctNumber = Random.Range(0, 4);
+                correctNumber = Random.Range(0, boxNumber);
                 text.text = "던전 목표: 키 찾기";
                 break;
             case QUEST.ALLKILL:
@@ -51,8 +61,10 @@ public class DungeonQuest : MonoBehaviour
                 
                 for (int i = 0; i < _allEnemys.Length; i++)
                 {
-                    if (_allEnemys[i].TryGetComponent<MonsterType>(out mt))
+                    Debug.Log(_allEnemys[i].name);
+                    if (_allEnemys[i].TryGetComponent(out mt))
                     {
+                        Debug.Log("monster");
                         maxMonster++;
                     }
                 }
@@ -62,9 +74,14 @@ public class DungeonQuest : MonoBehaviour
 
                 for (int i = 0; i < _allEnemys.Length; i++)
                 {
-                    if (_allEnemys[i].TryGetComponent<MonsterType>(out mt))
+                    Debug.Log(_allEnemys[i].name);
+                    if (_allEnemys[i].TryGetComponent(out mt))
                     {
-                        if (mt.GetEnemyType() == MonType.Melee) meleeMonster++;
+                        if (mt.GetEnemyType() == MonType.Melee)
+                        {
+                            Debug.Log("melee monster");
+                            meleeMonster++;
+                        }
                     }
                 }
                 break;
@@ -78,5 +95,24 @@ public class DungeonQuest : MonoBehaviour
         transform.GetChild(0).gameObject.SetActive(true);
 
         text.text = "던전 클리어!";
+    }
+
+    QUEST ChooseQuest()
+    {
+        /*
+            public bool keyQuest = false;
+            public bool allKillQuest = false;
+            public bool meleeAllKillQuest = false;
+        */
+        bool[] _ck = new bool[3] { keyQuest, allKillQuest, meleeAllKillQuest };
+
+        List<int> _list = new List<int>();
+
+        for(int i = 0; i < 3; i++)
+        {
+            if (_ck[i]) _list.Add(i);
+        }
+
+        return (QUEST)_list[Random.Range(0, _list.Count)];
     }
 }
