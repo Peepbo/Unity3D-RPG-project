0930a1076dc784423dd2429920a724b57a02d33a
DESKTOP-I4K35H7\PONYO <jeongyeon9722@gmail.com>
2021-01-13 03:30:49 +0900

    removed unnecessary script, functions and fixed profile bug in start scene

--- Unity3D RPG/Assets/Scripts/UI/MenuPopUp.cs
+++ Unity3D RPG/Assets/Scripts/UI/MenuPopUp.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class MenuPopUp : MonoBehaviour
 {
-    public GameObject menuPanel;
-    public GameObject menuIcon;
-    public GameObject achievementPanel;
-    public GameObject settingPanel;
     public GameObject quitPanel;
 
     // Update is called once per frame
@@ -27,48 +23,15 @@ public class MenuPopUp : MonoBehaviour
     {
         Time.timeScale = 0f; // 먼저 시간을 정지시킨다. (게임내 시간으로 게임 시간이 정지된다. 애니메이션 , 게임구동 관련 시간)
         // ↑ 이거 쓸지안쓸지 몰라서 일단 넣어놓았음.추후 수정 예정
-
-        menuPanel.SetActive(true);
-        menuIcon.SetActive(false);
     }
 
     public void MenuClose()
     {
         Time.timeScale = 1f;
-
-        menuPanel.SetActive(false);
-        menuIcon.SetActive(true);
-    }
-
-    public void AchievementPop()
-    {
-        achievementPanel.SetActive(true);
-        menuPanel.SetActive(false);
-    }
-
-    public void AchievementClose()
-    {
-        achievementPanel.SetActive(false);
-        menuPanel.SetActive(true);
     }
 
     public void QuitGame()
     {
         Application.Quit();
-    }
-
-    public void Continue()
-    {
-        quitPanel.SetActive(false);
-    }
-
-    public void SettingPop()
-    {
-        settingPanel.SetActive(true);
-    }
-
-    public void SettingClose()
-    {
-        settingPanel.SetActive(false);
     }
 }
