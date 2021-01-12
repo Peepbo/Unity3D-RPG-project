32b29febc87765e42f9c03f649f432d6c915f66b
DESKTOP-I4K35H7\PONYO <jeongyeon9722@gmail.com>
2021-01-12 15:57:47 +0900

    added UI animation in profile panel

--- Unity3D RPG/Assets/Scripts/UI/PanelAni.cs
+++ Unity3D RPG/Assets/Scripts/UI/PanelAni.cs
@@ -1,41 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PanelAni : MonoBehaviour
 {
-    Vector3 Nextposition;
+    public GameObject panel;
+    public bool isOpen;
 
-    // Start is called before the first frame update
-    void Start()
+    public void OpenPanelAni()
     {
+        panel.GetComponent<Animator>().SetBool("Open", true);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void ClosePanelAni()
     {
-    }
-
-    public void Up()
-    {
-        Nextposition = new Vector3(transform.position.x, transform.position.y + Screen.height, 0);
-        StartCoroutine(Movecamera());
-
-    }
-
-    public void Down()
-    {
-        this.transform.position = new Vector2(800, -460);
-    }
-
-    IEnumerator Movecamera()
-    {
-        yield return new WaitForSeconds(0.2f);
-        while (transform.position != Nextposition)
-        {
-            transform.position = Vector3.Lerp(transform.position, Nextposition, 8f * Time.deltaTime);
-            yield return new WaitForSeconds(0.02f);
-        }
+        panel.GetComponent<Animator>().SetBool("Open", false);
     }
 }
