8a2501f9292a76f13b85d5c13dbe11c48243e5d9
Harriet Lee <lharriet27@gmail.com>
2021-01-13 05:12:24 +0900

    create timeline script

---
+++ Unity3D RPG/Assets/Scripts/TimeLineController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class TimeLineController : MonoBehaviour
+{
+    public PlayableDirector director;
+    public TimelineAsset timeline;
+    public GameObject boss;
+    private void Start()
+    {
+        setPlay();
+    }
+
+    public void setPlay()
+    {
+        director.Play(timeline);
+    }
+}
