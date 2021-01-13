81de3e5f11cfca9593763bd6f924a1e02a75c207
Peepbo <tngus10778@gmail.com>
2021-01-13 11:34:23 +0900

    Dungeon 1 Merchant bug fix (add animation)

---
+++ Unity3D RPG/Assets/UiExit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiExit : MonoBehaviour
+{
+    public GameObject panel;
+    public void QuitButton()
+    {
+        gameObject.SetActive(false);
+        panel.SetActive(false);
+    }
+}
