57f09c358cde0b60217fd5e88e781c3328c37d60
DESKTOP-I4K35H7\PONYO <jeongyeon9722@gmail.com>
2021-01-14 12:10:58 +0900

    added die panel in UI scene

---
+++ Unity3D RPG/Assets/Scripts/UI/DiePanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiePanel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Die()
+    {
+        if (PlayerData.Instance.player.isDie == true)
+        {
+            this.gameObject.SetActive(true);
+        }
+    }
+}
