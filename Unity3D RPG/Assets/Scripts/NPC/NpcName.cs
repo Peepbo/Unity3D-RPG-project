62ed8b5f50c904e6bffb489032d3851f3a56e5db
채수현 <tngus1140@gmail.com>
2021-01-14 12:30:27 +0900

    NpcName float

---
+++ Unity3D RPG/Assets/Scripts/NPC/NpcName.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcName : MonoBehaviour
+{
+    public string name;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        gameObject.transform.LookAt(Camera.main.transform.position);
+        gameObject.transform.Rotate(0, 180, 0);
+        gameObject.GetComponent<TextMesh>().text = "" + name;
+    }
+}
