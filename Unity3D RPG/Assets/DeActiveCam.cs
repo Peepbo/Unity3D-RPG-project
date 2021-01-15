94ef318dc7e69618c8a0b82748744760e2001d20
Peepbo <tngus10778@gmail.com>
2021-01-15 17:27:07 +0900

    make Tutorial scene

---
+++ Unity3D RPG/Assets/DeActiveCam.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeActiveCam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ActiveTime());
+    }
+
+    IEnumerator ActiveTime()
+    {
+        yield return new WaitForSeconds(3.2f);
+        gameObject.SetActive(false);
+    }
+}
---
+++ Unity3D RPG/Assets/Scripts/Tutorial/TutorialMng.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialMng : MonoBehaviour
+{
+    bool isStart = false;
+    BoxCollider col;
+
+    public Chat chat;
+
+    public bool[] questCheck = new bool[7];
+
+    //shamen
+    public GameObject monster;
+
+    #region begin
+    private void Start()
+    {
+        col = GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            isStart = true;
+            col.enabled = false;
+            chat.NextQuest();
+
+            monster.SetActive(true);
+        }
+    }
+    #endregion
+}
