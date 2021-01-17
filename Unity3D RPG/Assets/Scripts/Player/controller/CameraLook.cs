7e074a15b01256ff7f79af7ca3401b1fcf14f97e
ahnsem824 <ahnsem96@gmail.com>
2021-01-17 18:40:20 +0900

    밸런스 수치조정
    TownScene 카메라 2개 사용 할수있도록 작업
    
    (쿼터뷰, 백뷰 선택가능 기능 생성 )
    쿼터뷰는 고정 백뷰는 회전가능 입니다

--- Unity3D RPG/Assets/Scripts/Player/controller/CameraLook.cs
+++ Unity3D RPG/Assets/Scripts/Player/controller/CameraLook.cs
@@ -11,6 +11,7 @@ public class CameraLook : MonoBehaviour
     private CinemachineFreeLook cinemachine;
     private MainPlayer playerInput;
     public TouchBoard touchBoard;
+    public CinemachineFreeLook cinemachine2;
 
     private void Awake()
     {
@@ -29,8 +30,17 @@ public class CameraLook : MonoBehaviour
 
     private void FixedUpdate()
     {
-        cinemachine.m_XAxis.Value += touchBoard.TouchDist.x * 15f * lookSpeed * Time.deltaTime;
-        cinemachine.m_YAxis.Value += -touchBoard.TouchDist.y * lookSpeed * Time.deltaTime/20 ;
+        if(PlayerData.Instance.isCameraBack)
+        {
+            cinemachine.m_XAxis.Value += touchBoard.TouchDist.x * 15f * lookSpeed * Time.deltaTime;
+            cinemachine.m_YAxis.Value += -touchBoard.TouchDist.y * lookSpeed * Time.deltaTime / 20;
+        }
+        else
+        {
+            //cinemachine2.m_XAxis.Value += touchBoard.TouchDist.x * 15f * lookSpeed * Time.deltaTime;
+
+        }
+
     }
     // Update is called once per frame
     void Update()
@@ -38,5 +48,18 @@ public class CameraLook : MonoBehaviour
         //Vector2 _delta = playerInput.PlayerMain.Look.ReadValue<Vector2>();
         
     }
+    public void CameraChange()
+    {
+        if(PlayerData.Instance.isCameraBack)
+        {
+            PlayerData.Instance.isCameraBack = false;
+            cinemachine2.enabled = true;
+        }
+        else
+        {
+            PlayerData.Instance.isCameraBack = true;
+            cinemachine2.enabled = false;
+        }
 
+    }
 }
--- Unity3D RPG/Assets/Scripts/Singleton/PlayerData.cs
+++ Unity3D RPG/Assets/Scripts/Singleton/PlayerData.cs
@@ -42,6 +42,10 @@ public partial class PlayerData : Singleton<PlayerData>
     public int currentAccessory = -1;
     public int currentBossItem = -1;
 
+
+
+    //카메라 쿼터인지 백인지
+    public bool isCameraBack;
     #region INSURANCE
     public bool normalInsurance;
     public bool rareInsurance;
