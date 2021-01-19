467e13c24a53ee0bf5fd240740d69707146d2ed8
Harriet Lee <lharriet27@gmail.com>
2021-01-19 15:30:06 +0900

    adjust script

--- Unity3D RPG/Assets/Scripts/Monster/EnemyMgr.cs
+++ Unity3D RPG/Assets/Scripts/Monster/EnemyMgr.cs
@@ -7,36 +7,37 @@ public abstract class EnemyMgr : MonoBehaviour
 {
     private IMoveAble moveType;
 
-    protected NavMeshAgent AI;
+    protected NavMeshAgent AI;                  //Monster AI Move용
     protected NavMeshPath path;
-    protected CharacterController controller;
-    protected Player player;                    //player script
+    protected CharacterController controller;   //충돌처리용
+    protected Player player;                    //player script l
     protected GameObject target;                //distance , direction 체크용
     protected Animator anim;
 
-    protected bool isHit;
-    protected bool isDamaged;
-    protected bool isDead;
-    protected float disappearTime;
+    protected bool isHit;                       //player 강공격 상태에서의 데미지 처리용
+    protected bool isDamaged;                   //GetDamage 제한용
+    protected bool isDead;                      //몬스터 사망처리
+    protected float disappearTime;              //사망 후 프리팹 비활성화 타임
 
-    public GameObject ItemBox;
+    public GameObject ItemBox;                  //몬스터가 드롭하는 Item정보를 담고 있을 ItemBox
 
 
     //stat
-    protected int maxHp;
-    protected int hp;
-    protected int atk;
-    protected float def;
-    public float speed;
+    protected int maxHp;            //최대 체력
+    protected int hp;               //현재 체력
+    protected int atk;              //공격력
+    protected float def;            //방어력
+    public float speed;             //NavMeshAgent 속도
 
-    public float angle;
+    public float angle;             //Goblin종족 시야범위 판정
     [Range(5, 30)]
-    public float findRange;
-    [Range(1, 10)]
-    public float attackRange;
+    public float findRange;         //타겟 확인 가능 범위
+    [Range(1, 10)] 
+    public float attackRange;       //공격 가능 범위
     [Range(1, 5)]
-    public float observeRange;
+    public float observeRange;      //Observe 기능을 사용하는 몬스터의 감시 범위
 
+    //Item
     protected int minGold;
     protected int maxGold;
     protected int currency;
@@ -75,6 +76,7 @@ public abstract class EnemyMgr : MonoBehaviour
 
     public void setMoveType(IMoveAble newMoveType)
     {
+        //IMoveAble 인터페이스 상속받아 구현된 스크립트를 받아온다.
         this.moveType = newMoveType;
     }
 
@@ -83,6 +85,7 @@ public abstract class EnemyMgr : MonoBehaviour
 
     protected IEnumerator LookBack(float time)
     {
+        //현재 흐르는 시간이 time이 될 때까지 타겟을 바라보도록 회전
         float t = 0f;
         while (t < time)
         {
@@ -93,7 +96,7 @@ public abstract class EnemyMgr : MonoBehaviour
             Quaternion _targetRotation = Quaternion.LookRotation(_direction);
 
             //Quaternion _nextRotation = Quaternion.Lerp(transform.localRotation, _targetRotation, Time.deltaTime);
-            Quaternion _nextRotation = Quaternion.Lerp(transform.localRotation, _targetRotation, t / 1.5f);
+            Quaternion _nextRotation = Quaternion.Lerp(transform.localRotation, _targetRotation, t / time);
 
             transform.localRotation = _nextRotation;
 
